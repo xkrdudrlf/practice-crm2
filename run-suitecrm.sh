c5c9f433380b531add0ed3ec8690726db41d94da
tom <tom@lesmills.com.au>
2024-06-25 17:27:36 +1000

    suitcrm is now running at least and phpadmin is linked to mysql

--- run-suitecrm.sh
+++ run-suitecrm.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-# Increate PHP memeory limit before 'composer install' to avoid potential "zip bomb" issue
-echo "memory_limit=512M" > /usr/local/etc/php/conf.d/memory-limit.ini
+echo "memory_limit=512M" > $PHP_INI_DIR/conf.d/memory-limit.ini
+echo "upload_max_filesize=20M" > $PHP_INI_DIR/conf.d/upload-size.ini
+
+# https://docs.suitecrm.com/8.x/admin/installation-guide/downloading-installing/#_1_4_configure_php_error_reporting
+sed -i 's/error_reporting = .*/error_reporting = E_ALL \& ~E_DEPRECATED \& ~E_STRICT \& ~E_NOTICE \& ~E_WARNING/g' "$PHP_INI_DIR/php.ini"
 
 # Run composer to install packages
 composer install
